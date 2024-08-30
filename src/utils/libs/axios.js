97a50e09fdd7364bbef4ed13df36417ebb549630
EAGLEMadaminov <javohirweb99@gmail.com>
2024-08-30 08:34:08 +0500

    check with production

--- src/utils/libs/axios.js
+++ src/utils/libs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = "https://vodiytezkoruz-aomcq.ondigitalocean.app";
-const url = "http://localhost:4000";
+const url = "https://vodiytezkoruz-aomcq.ondigitalocean.app";
+// const url = "http://localhost:4000";
 const axiosInstance = axios.create({
   baseURL: url,
   withCredentials: false,
